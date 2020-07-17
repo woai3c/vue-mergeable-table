bbe81896602766e82e12f462def945aa123d2053
woai3c <411020382@qq.com>
2020-07-17 22:11:03 +0800

    feat: 重新表格生成方式及调用方式

--- src/VueMergeableTable.vue
+++ src/VueMergeableTable.vue
@@ -1,5 +1,5 @@
 <script>
-export default { 
+export default {
     name: 'VueMergeableTable',
     props: {
         options: {
@@ -17,16 +17,14 @@ export default {
             rows: 0,
             listData: [],
             tableData: [],
-            merge: [],
         }
     },
     created() {
         this.cols = this.options && this.options.cols
         this.rows = this.options && this.options.rows
         this.listData = this.options && this.options.data
-        this.merge = this.options && this.options.merge
         this.tableData = this.createTableData()
-        this.addTableAttrs()
+        this.sort()
         this.mergeData()
     },
     methods: {
@@ -45,16 +43,20 @@ export default {
             return result
         },
 
-        addTableAttrs() {
-            const { rows, cols, merge, tableData } = this
-            for (let i = 0, len = merge.length; i < len; i++) {
-                const mergeOption = merge[i]
+        sort() {
+            const { rows, cols, tableData, listData } = this
+            for (let i = 0, len = listData.length; i < len; i++) {
+                const mergeOption = listData[i].merge
+                if (!mergeOption) continue
                 const rowStart = mergeOption.row
                 const colStart = mergeOption.col
                 
                 for (let row = 0; row < rows; row++) {
                     for (let col = 0; col < cols; col++) {
                         if (row == rowStart && col == colStart) {
+                            tableData[row][col] = { ...listData[i] }
+                            listData[i] = null
+                            
                             tableData[row][col].colspan = mergeOption.colspan? mergeOption.colspan : 1
                             tableData[row][col].rowspan = mergeOption.rowspan? mergeOption.rowspan : 1
                         }
@@ -70,15 +72,18 @@ export default {
             for (let i = 0, len = data.length; i < len; i++) {
                 const arr = data[i]
                 for (let j = 0, l = arr.length; j < l; j++) {
-                    const item = listData[index]
-                    if (!item) break
-                    if (arr[j].rowspan) item.rowspan = arr[j].rowspan
-                    if (arr[j].colspan) item.colspan = arr[j].colspan
-                    item.__index__ = index++
+                    if (arr[j].content) continue
+                    let item = listData[index]
+                    while (!item) {
+                        item = listData[++index]
+                        if (item === undefined) break
+                    }
+
                     arr[j] = item
+                    index++
                 }
             }
-
+            
             return data
         },
 
@@ -88,7 +93,6 @@ export default {
                 let arr = data[i]
                 if (!arr) continue
                 let j = 0
-                
                 while (j < arr.length) {
                     if (arr[j].rowspan && arr[j].colspan) {
                         this.deleteRowAndCol(data, i, arr[j].rowspan + i - 1, j, arr[j].colspan)
@@ -103,6 +107,8 @@ export default {
         deleteRowAndCol(data, rowStart, rowEnd, colStart, colspan) {
             const start = rowStart
             while (rowStart <= rowEnd) {
+                // 用于存储分成两截的数组的前段数据部分
+                const tempArry = []
                 let span = colspan
                 if (start === rowStart) {
                     span = colspan - 1
@@ -116,6 +122,7 @@ export default {
                 const arr = data[rowStart]
                 const arr2 = []
                 let len = arr.length
+                
                 // 大于 0 的情况下要从头开始
                 if (colStart >= len) {
                     let i = 0
@@ -135,21 +142,20 @@ export default {
                     let i = colStart
                     let isFirst = true
                     colStart--
-
                     for (; i != colStart; i = ++i % len) {
                         if (!span) {
                             if (i < colStart) {
-                                arr2.unshift(arr[i])
+                                tempArry.push(arr[i])
                             } else {
                                 arr2.push(arr[i])
                             }
 
                             continue
                         }
-                    
+                        
                         if (arr[i].rowspan && arr[i].colspan) {
                             if (i < colStart) {
-                                arr2.unshift(arr[i])
+                                tempArry.push(arr[i])
                             } else {
                                 arr2.push(arr[i])
                             }
@@ -164,7 +170,7 @@ export default {
                     }
                 }
 
-                data[rowStart] = arr2
+                data[rowStart] = [...tempArry, ...arr2]
                 rowStart++
             }
         },
@@ -172,7 +178,7 @@ export default {
         generateTable(h) {
             const data = this.replaceData()
             const result = []
-            let index = 0
+            let row = 0
             for (let i = 0, len = data.length; i < len; i++) {
                 const tdData = data[i]
                 const tds = []
@@ -187,10 +193,8 @@ export default {
                         attrs.rowspan = td.rowspan
                     }
 
-                    attrs['data-index'] = td.__index__
                     attrs['data-col'] = j
                     attrs.class = 'mergeable-td'
-
                     tds.push(
                         h('td', { attrs }, td.content)
                     )
@@ -199,19 +203,18 @@ export default {
                 result.push(h('tr', {
                     attrs: {
                         class: 'mergeable-tr',
-                        'data-index': index++,
+                        'data-row': row++,
                     },
                     on: {
                         click: (e) => {
                             const target = e.target
                             if (target.nodeName == 'TD') {
-                                const index = target.dataset.index
+                                const parent = target.parentNode
                                 this.$emit(
                                     'click',
-                                    this.listData[index],
-                                    index,
-                                    this.getTrData(target.parentNode),
-                                    target.parentNode.dataset.index,
+                                    this.tableData[parent.dataset.row][target.dataset.col],
+                                    this.getTrData(parent),
+                                    parent.dataset.row,
                                     target.dataset.col,
                                 )
                             }
@@ -224,10 +227,11 @@ export default {
         },
 
         getTrData(tr) {
+            const tableData = this.tableData
             const tds = tr.children
             const result = []
             for (let i = 0, len = tds.length; i < len; i++) {
-                result.push(this.listData[tds[i].dataset.index])
+                result.push(tableData[tr.dataset.row][tds[i].dataset.col])
             }
 
             return result
